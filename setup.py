15060adfa89a218799dc888ffc1186781f415e78
xiangyan93 <yan.xiang@duke.edu>
2022-08-15 15:57:51 -0400

    bugfix.

--- setup.py
+++ setup.py
@@ -44,5 +44,5 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     include_package_data=True,
-    data_files=[('hyperparameters', ['mgktools/hyperparameters/additive.json'])]
+    data_files=[('hyperparameters', ['mgktools/hyperparameters/*.json'])]
 )
